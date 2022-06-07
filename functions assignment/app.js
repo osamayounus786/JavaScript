de297080d0a126005c59f1068f37052db745bf9d
Osama Younus <osamayounus541@gmail.com>
2022-06-07 13:48:04 +0500

    IFELSE and Function assignment added

---
+++ functions assignment/app.js
@@ -0,0 +1,150 @@
+// var ElDate = document.querySelector("#cDate");
+// var showDate = document.querySelector("#message");
+
+// function DateBrowser(){
+//     showDate.innerHTML
+// }
+// var showMessage = document.querySelector("#showLearn");
+
+// showMessage.innerHTML = "You are learning JAVascript";
+
+
+
+
+// var today = new Date();
+//   var day = today.getDay();
+//   var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+//   var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+//   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//   var dateTime = date+' '+time;
+   
+//   document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+
+
+
+
+
+// 3.	Write a function that takes first & last name
+//  and then it greets the user using his full name.
+
+// var firstName = document.querySelector("#firstName");
+// var LastName = document.querySelector("#lastName");
+
+// var showGreetings = document.querySelector("#showGreetings");
+
+// function greetings() {
+//   showGreetings.innerHTML = "With Greeting " + firstName.value + "" + LastName.value;
+//   clearfunc();
+// }
+// function clearfunc(){
+// firstName.value = "";
+// LastName.value = "";
+// }
+// 4.	Write a function that adds two numbers (input by user) and returns the sum of two numbers.
+// var num1 = prompt("Insert Num1");
+// var num2 = prompt("Insert Num2");
+
+// var inpuNumOne = document.querySelector("#num1");
+// var inpuNumTwo = document.querySelector("#num2");
+// var showResult = document.querySelector("#message");
+//  function calcTotalPercent() {
+//     //  var htmlMarks = +prompt("what is your HTML marks?");
+//     //    var cssMarks = +prompt("what is your CSS marks?");
+  
+//        var obtainedMarks = inpuNumOne + inpuNumTwo;
+//       //  var percentage = obtainedMarks / 200 * 100;
+  
+//        console.log(obtainedMarks);
+//    }
+
+
+
+// var h1 = document.querySelector("h1");
+// console.log(h1);
+
+// Write a function that takes three arguments num1, num2 & operator & compute the desired operation. Return and show the desired result in your browser.
+
+// function calcu(){
+//     var num1 = +prompt("Inser Num 1");
+//     var operater = prompt("Insert operater");
+//     var num2 = +prompt("Inser Num 2");
+
+//     calculate = num1 + operater + num2;
+//     console.log(calculate);
+
+
+
+// }
+// Write a function that takes three arguments num1, num2 & operator & compute the desired operation. Return and show the desired result in your browser.
+
+
+// function calcu(){
+//      var num1 = +prompt("Inser Num 1");
+//      var operater = prompt("Insert operater");
+//      var num2 = +prompt("Inser Num 2");
+
+//      if(operater === '+'){
+//          resultofsum = num1 + num2;
+//          console.log(resultofsum);
+//         }
+//         else if (operater === '-'){
+//             resultofsum = num1 - num2;
+//             console.log(resultofsum);  
+//         }
+//         else if (operater === '*') {
+//             resultofsum = num1 * num2;
+//             console.log(resultofsum);
+
+//         }
+//         else if (operater === '/') {
+//             resultofsum = num1 / num2;
+//             console.log(resultofsum);
+
+//         }
+//         else {
+//             console.log("Error");
+//             console.log(resultofsum);
+//         }
+       
+
+
+
+//  }
+
+
+
+
+// Write a function that squares its argument.
+
+// var showSquare = document.querySelector("#message");
+// function squareofNum(){
+// var insNum = prompt("Insert a number to square it");
+
+// showSquare.innerHTML = (insNum ** 2);
+// console.log(insNum ** 2);
+// }
+// 10.	Write a function that writes variable length arguments list in your browser.
+
+//  var variableOfCon = document.querySelector("#variable");
+//  var showLenght = document.querySelector("#showLenght");
+
+//  function knowLenght(){
+
+//     var myVariable = variableOfCon.value;
+//     lenghtVariable = myVariable. lenght;
+//     //  console.log(myVariable. lenght);
+//       console.log(lenghtVariable.value);
+    
+//     // showLenght.innerHTML = variableOfCon.value;
+//     // variableOfCon.value = showLenght.innerHTML.length;
+//     // console.log(variableOfCon.value);
+//  }
+// var myString = "string test";
+// var stringLength = myString. length;
+// console. log(stringLength); // Will return 11 because myString.
+
+// var num1 = Math.max [34,4,54,34,123,67] ;
+// console.log(num1);
+
+
+
---
+++ iflese assignment/app.js
@@ -0,0 +1,33 @@
+
+// Write a program to check whether the given input number is
+// divisible by 3 or else show a message “Number is not divisible
+// by 3”.
+// inNum = prompt("Insert a number");
+//  if(inNum % 2 == 0 ){
+//      console.log("number is divisivle")
+
+//  }
+//  else{
+//      console.log("number is not divisible");
+//  }
+
+// Write a program that checks whether the given input is an even
+// number or an odd number.
+// var num = prompt("Insert a number to checkout");
+// if(num % 2 == 0){
+// console.log("the number is even");
+// }
+// else{
+//     console.log("the number is odd");
+// }
+// Write an if/else statement with the following condition:
+// If the variable age is greater than 18, output "Old enough",
+// otherwise output "Too young".
+var age = 18;
+var inAge = prompt("Your Age");
+if (age >= inAge){
+console.log("your are old enough");
+}
+else{
+    console.log("too young");
+}
