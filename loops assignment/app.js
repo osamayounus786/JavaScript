43c8d2070e930fe0a417c1307d86e8038e8fddf0
Osama Younus <osamayounus541@gmail.com>
2022-06-08 13:25:58 +0500

    Loop Assignments

---
+++ loops assignment/app.js
@@ -0,0 +1,35 @@
+// 1. Write a program to display the message “Hello World” 5 times
+// in your browser using for loop.
+// for(i = 0; i <= 5; i++){
+// console.log("Hello World!" , + i)
+// }
+
+
+
+// Write a program to print numeric counting from 1 to 10.
+// for(i = 1; i <=10; i++){
+//     console.log(i);
+// }
+
+// . Write a program to print multiplication table of any number
+// using for loop. Table number & length should be taken as an
+// input from user
+
+// tableName = +prompt("Enter Table Name");
+// tableLenght = +prompt("Enter the lenght of table");
+
+// for(tableTime = 1; tableTime <  tableLenght + 1; tableTime++){
+//     console.log(tableName + " X " + tableTime + " = " + tableName * tableTime);
+// }
+
+// 4. You have an array
+// A = [“Nokia”, “Samsung”, “Apple”, “Sony”, “Huawei”]
+// Write each element on new line with the help of for loop.
+
+// var A = ["Nokia", "Samsung", "Apple", "Sony", "Huawei"];
+// // console.log(A);
+
+// for(i = 0; i < A.length; i++){
+
+//     console.log(i + [A]);
+// }
