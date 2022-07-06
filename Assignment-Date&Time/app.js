1aa42b18489d095aa1877ef1df0ba590ba2e58f2
Osama M.younus <osamayounus541@gmail.com>
2022-07-06 15:27:40 +0500

    assignments

---
+++ Assignment-Date&Time/app.js
@@ -0,0 +1,83 @@
+// 1. Write a program that takes a positive integer from user &
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number 
+
+
+
+// not a perfect code for practice 
+// var number = prompt("write a number");
+//  number = prompt("write a number to round off");
+//  number = prompt("write a number to floor");
+//  number = prompt("write a number to ceil");
+//   roundNumber =  Math.round(number);
+//   roundNumber =  Math.floor(number);
+//   roundNumber =  Math.ceil(number);
+//  document.write(number);
+//  document.write(" Round off ",roundNumber + "< /br>");
+//  document.write(" Floor method ",roundNumber);
+//  document.write(" Ceil Mehod ",roundNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write a program that stores a random secret number from 1
+// to 10 in a variable. Ask the user to input a number between 1
+// and 10. If the user input equals the secret number, congratulate
+// the user
+
+
+
+
+// var userInput = +prompt("Write a number to find out matches random number or not?")
+// var ranNum = Math.ceil( Math.random() * 10);
+// if(userInput === ranNum){
+// document.write("Congrats Number matches ",userInput,ranNum);
+// }
+// else{
+//     document.write("Loss try again Didn't Match ","Number You input: ",userInput, " &" , " Random number: ",ranNum);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
