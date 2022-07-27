e5d71e1be57586b8127bcba3122101bc7004807b
Osama M.Younus <osamayounus541@gmail.com>
2022-07-27 19:41:56 +0500

    While looop completed

---
+++ JSClass While & Do While Loop/app.js
@@ -0,0 +1,20 @@
+// var i = 0;
+// while( i <=10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// var i = 0;
+// while (i !== 10){
+//     var randomNum = Math.ceil(Math.random() * 10);
+//     console.log(i);
+//     i = randomNum;
+    
+// }
+var i = 1;
+while (i % 2 !== 0) {
+    var ranNum = Math.ceil((Math.random() * 10));
+    console.log(ranNum);
+    i = ranNum;
+}
