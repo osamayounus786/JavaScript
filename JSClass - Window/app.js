a5ac2d713740f6fb6c9a57e61404ef36e4a8786b
Osama M.Younus <osamayounus541@gmail.com>
2022-08-16 19:57:35 +0500

    windows tasks to do

---
+++ JSClass - Window/app.js
@@ -0,0 +1,19 @@
+var whereWeAt = window.location.href;
+console.log(whereWeAt);
+
+var whereWeAt = window.location.hostname;
+console.log(whereWeAt);
+
+var whereWeAt = window.location.pathname;
+console.log(whereWeAt);
+
+// window.location.reload(true);
+// window.location.reload(false);
+// window.location.reload();
+// history.back();
+// history.forward();
+var monkeyWindow = window.open("https://www.google.com");
+console.log(monkeyWindow);
+// var windowContent =
+//   "<h1>Capuchin monkey</h1><img src= 'monkey.jpg'><p>The word capuchin derives from a group of friars<br>named the Order of Friars Minor Capuchin who wear<br>brown robes with large hoods covering their heads.</p>";
+// console.log(windowContent);
