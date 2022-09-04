8784772bd26bd81689fe718e20dbd0503703b2b8
Osama M.Younus <osamayounus541@gmail.com>
2022-09-04 20:18:51 +0500

    switch case

--- JSClass-Function/app.js
+++ JSClass-Function/app.js
@@ -68,29 +68,113 @@
 
 
 
-var a = 10;
-var b = 15;
-var Calcu = prompt("Write mathematics operater to do operation of a & b");
-switch (Calcu){
-    case "+":
-    alert(a + b);
-    break;
-    case "-":
-        alert(a - b);
-        break;
-        case "*":
-    alert(a * b);
-    break;
-    case "Wed":
-        alert("Its is wednesday");
-        break;
-    case "Thu":
-        alert("Its is Thursday");
-        break;
-    case "Fri":
-        alert("Its is Friday");
-        break;
-    case "Sat":
-        alert("Its is Saturday");
-        break;
-}
+// var a = 10;
+// var b = 15;
+// var Calcu = prompt("Write mathematics operater to do operation of a & b");
+// switch (Calcu){
+//     case "+":
+//     alert(a + b);
+//     break;
+//     case "-":
+//         alert(a - b);
+//         break;
+//         case "*":
+//     alert(a * b);
+//     break;
+//     case "Wed":
+//         alert("Its is wednesday");
+//         break;
+//     case "Thu":
+//         alert("Its is Thursday");
+//         break;
+//     case "Fri":
+//         alert("Its is Friday");
+//         break;
+//     case "Sat":
+//         alert("Its is Saturday");
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var namePerson = document.getElementById('suleman');
+
+
+// function newFunc(){
+//     console.log("this is suleman the shahka");
+//     namePerson.innerHTML = "this is saqib the gattae builder"
+// }
+// newFunc();
+
+
+
+
+
+
+
+// function greetUser(greeting){
+//     greeting = 'hello there'
+//     shabana = 'salam'
+//     alert(greeting + shabana);
+// }
+// greetUser('hello there',18);
+
+
+// var myname = 'osama';
+// var fName = 'M.Younus';
+
+// function greetUser(myname,fName){
+//     alert(myname + fName);
+// }
+// // greetUser(myname,fName);
+// greetUser('saqib ','abbas');
+
+var merchTot = parseInt(prompt("write number"));
+
+var orderTot;
+ if (merchTot >= 100) {
+ orderTot = merchTot;
+ console.log(orderTot);
+ }
+ else if (merchTot < 50.01) {
+ orderTot = merchTot + 5;
+ console.log(orderTot);
+ }
+ else {
+ orderTot = merchTot + 5 + (.03 * (merchTot - 50));
+ console.log(orderTot);
+ }
+ 
