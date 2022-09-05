b0b58da2120935e2738c85ee6fbe82a4be7df1b1
Osama M.Younus <osamayounus541@gmail.com>
2022-09-05 13:12:31 +0500

    try and catch clear

--- JS-Try & Catch/app.js
+++ JS-Try & Catch/app.js
@@ -1,6 +1,43 @@
-try{
-    aler("this is error to catch");
-}catch(e){
-    console.log('this is not error');
+// try{
+//     aler("this is error to catch");
+// }catch(e){
+//     console.log('this is not error');
 
-}
+// }
+
+
+// console.log(sUleman);
+// console.log(sulEman);
+
+
+// aler("this is error");
+// console.log("pakistan");
+
+
+
+// try{
+//     aler("this is error");
+// }catch(e){
+//     console.log(e);
+// }
+
+
+//  var userEmail = "abc@gmail.com";
+//  var passCode = "abcd@123456";
+//  var emailInput = prompt('write your email');
+//  var passCodeInput = prompt('what is passcode?');
+//  try {
+//      if (emailInput === userEmail && passCodeInput === passCode) {
+//          console.log('Authenticated');
+//      }
+//      else {
+//          throw "Email or pass code is incorrect."
+//      }
+//  } catch (e) {
+//      console.error(e);
+//      document.write(e);
+//  }
+
+
+
+
