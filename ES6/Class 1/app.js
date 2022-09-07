2384c777fee821c281b0a8e5e7508c2cd8bd3b90
Osama M.Younus <osamayounus541@gmail.com>
2022-09-07 13:55:10 +0500

    ES6 Started

---
+++ ES6/Class 1/app.js
@@ -0,0 +1,34 @@
+// var userName = "Osama";
+
+// function greetUser(){
+//     let userName = "Younus";
+// console.log(userName);
+// }
+// console.log(userName);
+// greetUser();
+
+
+// var user = "osama";
+// var user = "hello"
+// console.log(user);
+
+
+
+// how to concatenate in js advance
+    // here it is basics JS 
+// let firstName = "Osama";
+// let LastName = "younus";
+//  let fullName = firstName + LastName;
+//  console.log(fullName);
+
+//  here it is JS Advance
+
+// let firstName = "osama";
+// let LastName = "Younus";
+// console.log(`${firstName} ${LastName}`);
+
+
+
+let firstName = prompt("Your First Name");
+let LastName = prompt("Your last Name");
+console.log(`${firstName} ${LastName}`);
--- JSClass - Window/app.js
+++ JSClass - Window/app.js
@@ -1,19 +1,52 @@
-var whereWeAt = window.location.href;
-console.log(whereWeAt);
+// var whereWeAt = window.location.href;
+// console.log(whereWeAt);
 
-var whereWeAt = window.location.hostname;
-console.log(whereWeAt);
+// var whereWeAt = window.location.hostname;
+// console.log(whereWeAt);
 
-var whereWeAt = window.location.pathname;
-console.log(whereWeAt);
+// var whereWeAt = window.location.pathname;
+// console.log(whereWeAt);
 
 // window.location.reload(true);
 // window.location.reload(false);
 // window.location.reload();
 // history.back();
 // history.forward();
-var monkeyWindow = window.open("https://www.google.com");
-console.log(monkeyWindow);
+// var monkeyWindow = window.open("https://www.google.com");
+// console.log(monkeyWindow);
 // var windowContent =
 //   "<h1>Capuchin monkey</h1><img src= 'monkey.jpg'><p>The word capuchin derives from a group of friars<br>named the Order of Friars Minor Capuchin who wear<br>brown robes with large hoods covering their heads.</p>";
 // console.log(windowContent);
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(window.location.href);
+console.log(window.location.host);
+console.log(window.location.hostname);
+console.log(window.location.pathname);
+console.log(window.location.hash);
+
+window.location.href = ("https://www.google.com/");
+window.location.replace = https://www.google.com/
+
+
+
+
+
+
+
+
+
+
+
+
+
