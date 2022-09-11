fd4d13aea7f417ef563d1167b7f1561a2c7d5400
Osama M.Younus <osamayounus541@gmail.com>
2022-09-11 20:13:35 +0500

    ...

--- ES6/Class array loops/app.js
+++ ES6/Class array loops/app.js
@@ -1,4 +1,4 @@
-/* const user = ['apple','Mango','Banana'];
+ const user = ['apple','Mango','Banana'];
 
 
 for(i=0; i<user.length; i++){
@@ -9,11 +9,29 @@ for(i=0; i<user.length; i++){
 user.forEach((element)=>{
     console.log(element);
 })
-*/
-/*
+
+
 let veg = ['Tomato','Carrot','Onion','Chilli','Potato'];
 
 veg.forEach((index)=>{
     console.log(index);
 })
-*/
+
+
+//array from
+
+let arr = 'Osama';
+let newArr = Array.from(arr);
+console.log(newArr);
+
+
+// for of
+
+let num = ['1','2','3','4'];
+for(let i of num){
+    console.log(i);
+}
+        console.log('this is for in');
+for(let newI in num){
+    console.log(num[newI]);
+}
