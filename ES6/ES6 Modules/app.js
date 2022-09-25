faa284f5de6b9909debcb2a1685e912d55421d0d
Osama M.Younus <osamayounus541@gmail.com>
2022-09-25 13:26:25 +0500

    modules

---
+++ ES6/ES6 Modules/app.js
@@ -0,0 +1,18 @@
+ let userName = 'Sam';
+
+
+
+ function callMe(){
+    console.log('Hello world');
+}
+
+
+
+
+ class Pname{
+    constructor(){
+        console.log('ES6 Modules');
+    }
+}
+
+export {userName,callMe,Pname};
---
+++ ES6/ES6 Modules/new.js
@@ -0,0 +1,12 @@
+import { userName as nayaName,callMe,Pname  } from "./app.js";
+
+console.log(nayaName);
+
+
+
+
+callMe();
+
+
+
+let a = new Pname();
