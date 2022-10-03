ccb1f5a9a6a56e5615876532b43d57e977345589
Osama M.Younus <osamayounus541@gmail.com>
2022-10-03 14:02:37 +0500

    AJAX cocepts completed

---
+++ ES6/AJAX/app.js
@@ -0,0 +1,19 @@
+
+
+
+function loaddata(){
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+
+            // console.log(this.responseText);
+            document.getElementById("load").innerHTML = this.responseText;
+        }
+    }
+
+
+
+    xhttp.open('GET',"readme.txt",true);
+    xhttp.send();
+}
---
+++ ES6/AJAX/readme.txt
@@ -0,0 +1,1 @@
+Hello this is osama front-end developer
