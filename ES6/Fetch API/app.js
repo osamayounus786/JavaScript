d35f44c774f69dd7307f25dcebee494ffda7a438
Osama M.Younus <osamayounus541@gmail.com>
2022-10-11 13:32:00 +0500

    getting data from an object

--- ES6/Fetch API/app.js
+++ ES6/Fetch API/app.js
@@ -1,13 +1,28 @@
 
 
-fetch("readme.txt") // your content file is here i
-.then((response)=> response.text())
-.then((data)=> document.write(data)
-);
+// fetch("readme.txt") // your content file is here i
+// .then((response)=> response.text())
+// .then((data)=> document.write(data)
+// );
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/posts") // fake JSON file format
+// .then((response)=> response.json())
+// .then((data)=> console.log(data)
+// );
 
 
 
 fetch("https://jsonplaceholder.typicode.com/posts") // fake JSON file format
 .then((response)=> response.json())
-.then((data)=> console.log(data)
+.then((data)=> {
+    console.log(data);
+    for(var x in data){
+            
+            console.log(x)
+            console.log(`${data[x].title}`)
+            
+    }
+}
 );
