5d7c10296aca48006bf0baeffe8e402865d66055
Osama M.Younus <osamayounus541@gmail.com>
2022-10-14 19:51:58 +0500

    completed

--- ES6/Async & Await/app.js
+++ ES6/Async & Await/app.js
@@ -1,7 +1,27 @@
-async function greetings(){
-    return "hello";
-}
- greetings()
- .then((result) =>{
-        console.log(result);
- })
+// async function greetings(){
+//     return "hello";
+// }
+//  greetings()
+//  .then((result) =>{
+//         console.log(result);
+//  })
+
+
+
+
+
+
+ async function salam(){
+      try{
+          await  console.log("Walaikum Salam");
+          console.log("B")
+        }
+        catch(e){
+            console.log(e);
+        }
+        console.log("C")
+    }
+    salam()
+    console.log("D")
+
+ 
